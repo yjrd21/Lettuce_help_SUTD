ef43c3b5db8018b629e5234dedaa91ce2efbe107
Daniel Yuen <daniel_yuen@mymail.sutd.edu.sg>
2023-04-01 07:22:56 +0800

    Users are now able to be uploaded to our database as objects

--- app/src/main/java/com/example/loginregisterfirebase/Register.java
+++ app/src/main/java/com/example/loginregisterfirebase/Register.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,9 +26,10 @@ public class Register extends AppCompatActivity {
 
         final EditText fullname = findViewById(R.id.fullname);
         final EditText email = findViewById(R.id.email);
-        final EditText phone = findViewById(R.id.phone);
+        final EditText phone = findViewById(R.id.userid);
         final EditText password = findViewById(R.id.password);
         final EditText conPassword = findViewById(R.id.conpassword);
+        final Switch staffquery= findViewById(R.id.staffquery);
 
         final Button registerBtn = findViewById(R.id.registerBtn);
         final TextView loginNowBtn = findViewById(R.id.loginNow );
@@ -35,56 +37,9 @@ public class Register extends AppCompatActivity {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // get data fron EditTexts into String variables
-                final String fullnameTxt = fullname.getText().toString();
-                final String emailTxt = email.getText().toString();
-                final String phoneTxt = phone.getText().toString();
-                final String passwordTxt = password.getText().toString();
-                final String conPsswordTxt = conPassword.getText().toString();
-
-                //check if user fill all the fields before sending data to firebase
-                if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty() || passwordTxt.isEmpty()){
-                    Toast.makeText(Register.this,"Please fill all fields",Toast.LENGTH_SHORT).show();
-
-                }
-
-                // check if passwords are matching with each other
-                // if not matching with each other then show a toast
-                else if (!passwordTxt.equals(conPsswordTxt)){
-                    Toast.makeText(Register.this,"Passwords do not match", Toast.LENGTH_SHORT).show();
-                }
-
-                else{
-
-                    databaseReference.child("Volunteer").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            if (snapshot.hasChild(phoneTxt)) {
-                                Toast.makeText(Register.this, "Phone is already registered", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-
-                                databaseReference.child("Volunteer").child(phoneTxt).child("fullname").setValue(fullnameTxt);
-                                databaseReference.child("Volunteer").child(phoneTxt).child("email").setValue(emailTxt);
-                                databaseReference.child("Volunteer").child(phoneTxt).child("password").setValue(passwordTxt);
-
-                                Toast.makeText(Register.this,"User registered successfully", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-
-
-                }
-
+                InsertUserData(fullname,email,phone,password,conPassword,staffquery);
             }
         });
-
 
         loginNowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,5 +48,63 @@ public class Register extends AppCompatActivity {
             }
         });
 
+
     }
+    private void InsertUserData(EditText fullname,EditText email,EditText phone,EditText password,EditText conPassword,Switch staffquery){
+
+        // get data fron EditTexts into String variables
+        final String fullnameTxt = fullname.getText().toString();
+        final String emailTxt = email.getText().toString();
+        final String phoneTxt = phone.getText().toString();
+        final String passwordTxt = password.getText().toString();
+        final String conPsswordTxt = conPassword.getText().toString();
+
+
+
+        //check if user fill all the fields before sending data to firebase
+        if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty() || passwordTxt.isEmpty()){
+            Toast.makeText(Register.this,"Please fill all fields",Toast.LENGTH_SHORT).show();
+        }
+
+        // check if passwords are matching with each other
+        // if not matching with each other then show a toast
+        else if (!passwordTxt.equals(conPsswordTxt)){
+            Toast.makeText(Register.this,"Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+
+            User user= new User(fullnameTxt,emailTxt,phoneTxt,passwordTxt,conPsswordTxt,"Volunteer");
+
+            databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.hasChild("user")) {
+                        Toast.makeText(Register.this, "User is already registered", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+
+                        if (staffquery.isChecked()){
+                            User userstaff= new User(fullnameTxt,emailTxt,phoneTxt,passwordTxt,conPsswordTxt,"Staff");
+                            databaseReference.push().setValue(userstaff);
+                        }
+                        else{
+                            databaseReference.push().setValue(user);
+                        }
+                        Toast.makeText(Register.this,"User registered successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+
+        }
+    }
+
+
 }
